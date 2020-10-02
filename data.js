d6c41175dda7f6d89e099d38a8e3b2496f8524a5
Jaydev Patel <jaydev.patel@ad.infosys.com>
2020-10-02 15:46:44 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,122 @@
+var cdioverrideedits = [
+    {
+        "RuleLevel": "Comapny Fleet",
+        "CustomerName": "PHILLI66 (14184)",
+        "CityName": "",
+        "State": "",
+        "Fleet": "H01",
+        "Type": "Value",
+        "Overage": "200",
+        "Maximum": "200",
+        "LstMaintId": "B012936",
+        "LstMaintTS": "2017-08-09 15:43.49.657699",
+        "Comments": ""
+    },
+    {
+        "RuleLevel": "Company Fleet",
+        "CustomerName": "SKYLINSTEFAB (6381)",
+        "CityName": "",
+        "State": "",
+        "Fleet": "M01",
+        "Type": "Value",
+        "Overage": "4",
+        "Maximum": "4",
+        "LstMaintId": "B744625",
+        "LstMaintTS": "2013-12-17 09:52.49.657699",
+        "Comments": ""
+    },
+    {
+        "RuleLevel": "Customer",
+        "CustomerName": "GOGGINWARLLC (1918)",
+        "CityName": "DALLAS",
+        "State": "TX",
+        "Fleet": "",
+        "Type": "Percentage",
+        "Overage": "110",
+        "Maximum": "200",
+        "LstMaintId": "ED0DC358",
+        "LstMaintTS": "2018-01-24 09:38.32.657699",
+        "Comments": ""
+    },
+    {
+        "RuleLevel": "Customer",
+        "CustomerName": "USMINERALS (14330)",
+        "CityName": "",
+        "State": "",
+        "Fleet": "",
+        "Type": "Percentage",
+        "Overage": "200",
+        "Maximum": "200",
+        "LstMaintId": "B744625",
+        "LstMaintTS": "2015-01-19 15:43.49.657699",
+        "Comments": "9/16/14 ok corey ellis"
+    }
+];
+
+var enforcefleetedits = [
+    {
+        "Rule Level": "Fleet",
+        "Customer Name": "BSBTIE (11685)",
+        "City Name": "SPRINGFIELD",
+        "State": "MO",
+        "Fleet": "M01",
+        "Type": "Value",
+        "Overage": "20",
+        "Maximum": "20",
+        "Lst Maint Id": "B155759",
+        "Lst Maint TS": "2014-02-21 12.21.00.000000",
+        "Comments": ""
+    },
+    {
+        "Rule Level": "Fleet",
+        "Customer Name": "GROSSJANES (551)",
+        "City Name": "AMORY",
+        "State": "MS",
+        "Fleet": "M01",
+        "Type": "Value",
+        "Overage": "20",
+        "Maximum": "20",
+        "Lst Maint Id": "B155759",
+        "Lst Maint TS": "2014-02-21 12.21.00.000000",
+        "Comments": ""
+    },
+    {
+        "Rule Level": "Fleet",
+        "Customer Name": "GROSSJANES (551)",
+        "City Name": "Carthage",
+        "State": "TX",
+        "Fleet": "M01",
+        "Type": "Value",
+        "Overage": "20",
+        "Maximum": "20",
+        "Lst Maint Id": "B155759",
+        "Lst Maint TS": "2014-02-21 12.21.00.000000",
+        "Comments": ""
+    },
+    {
+        "Rule Level": "Fleet",
+        "Customer Name": "GROSSJANES (551)",
+        "City Name": "CULLENOAL",
+        "State": "AR",
+        "Fleet": "M01",
+        "Type": "Value",
+        "Overage": "20",
+        "Maximum": "20",
+        "Lst Maint Id": "B155759",
+        "Lst Maint TS": "2014-02-21 12.21.00.000000",
+        "Comments": ""
+    },
+    {
+        "Rule Level": "Fleet",
+        "Customer Name": "MIDCOLLBRPRO (794)",
+        "City Name": "MADRAS",
+        "State": "OR",
+        "Fleet": "F04",
+        "Type": "Percentage",
+        "Overage": "130",
+        "Maximum": "130",
+        "Lst Maint Id": "B155759C",
+        "Lst Maint TS": "2013-06-19 15.57.22.876752",
+        "Comments": ""
+    }
+]
